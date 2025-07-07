2add194aa30ec45d294e45bc2e95a728aafe50dd
EZBurgin <eduardozanderburgin@gmail.com>
2025-07-07 18:52:54 -0300

    :rocket: Validação dos dados do Usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -6,17 +6,42 @@ update - Atualizar
 delete - Deletar
 */
 import { v4 } from 'uuid'
+import * as Yup from 'yup'
 
 import User from '../models/User.js'
 
 class UserController {
     async store(req, res) {
-        const {name, email, password_hash, admin} = req.body
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().min(6).required(),
+            admin: Yup.boolean()
+        })
+
+        try {
+            schema.validateSync(req.body, { abortEarly: false })
+        } catch (err) {
+            return res.status(400).json({ error: err.errors })
+        }
+
+        const { name, email, password_hash, admin } = req.body
+
+        const userExists = await User.findOne({ 
+            where: {
+                email
+            }
+         })
+
+        if (userExists) {
+            return res.status(400).json({ error: 'User already exists' })
+        }
+
         const user = await User.create({
             id: v4(),
-            name  ,
-            email ,
-            password_hash ,
+            name,
+            email,
+            password_hash,
             admin
         })
 
