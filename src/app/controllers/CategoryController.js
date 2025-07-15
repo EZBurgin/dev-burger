3eedfd4ee80544c407da8d87722338c6bc141091
EZBurgin <eduardozanderburgin@gmail.com>
2025-07-15 19:48:10 -0300

    :white_check_mark: Update nas Categorias

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -20,6 +20,7 @@ class CategoryController {
             return res.status(401).json()
         }
 
+        const { filename: path } = req.file
         const { name } = req.body
 
         const categoryExists = await Category.findOne({
@@ -31,7 +32,8 @@ class CategoryController {
         }
 
         const { id } = await Category.create({
-            name
+            name,
+            path
         })
 
         return res.status(201).json({ id, name })
@@ -41,6 +43,67 @@ class CategoryController {
         const categories = await Category.findAll()
 
         return res.json(categories)
+    }
+
+    async update(req, res) {
+        const schema = Yup.object({
+            name: Yup.string()
+        })
+
+        try {
+            schema.validateSync(req.body, { abortEarly: false })
+        } catch (err) {
+            return res.status(400).json({ error: err.errors })
+        }
+
+        const { admin: isAdmin } = await User.findByPk(req.userId)
+
+        if (!isAdmin) {
+            return res.status(401).json()
+        }
+
+        const { id } = req.params
+
+        const categoryExists = await Category.findByPk(id)
+
+        if (!categoryExists) {
+            return res.status(400).json({ message: 'Make sure your category id is correct' })
+        }
+
+        let path
+
+        if (req.file) {
+            path = req.file.filename
+        }
+
+        const { name } = req.body
+
+
+        if (name) {
+            const categoryNameExists = await Category.findOne({
+                where: {
+                    name
+                }
+            })
+
+            if (categoryNameExists && categoryNameExists.id !== +id) {
+                return res.status(400).json({ error: 'Category already exists ' })
+            }
+        }
+
+        await Category.update({
+            name,
+            path
+        },
+            {
+                where: {
+                    id
+                }
+            }
+        )
+
+        return res.status(200).json()
+
     }
 
 }
